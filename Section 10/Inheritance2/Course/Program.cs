059cfab2038da21635aa67b46cd8cfbc5970f482
Gabriel Malheiro <g.silva.malheiro@avanade.com>
2023-04-12 10:53:20 -0300

    Updated  netcoreapp to 6.0

--- Section 10/Inheritance2/Course/Program.cs
+++ Section 10/Inheritance2/Course/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
 using Course.Entities;
 
 namespace Course
@@ -7,14 +9,32 @@ namespace Course
     {
         static void Main(string[] args)
         {
-            Account acc1 = new Account(1001, "Alex", 500.0);
-            Account acc2 = new SavingsAccount(1002, "Anna", 500.0, 0.01);
+            List<Account> list = new List<Account>();
 
-            acc1.Withdraw(10.0);
-            acc2.Withdraw(10.0);
+            list.Add(new SavingsAccount(1001, "Alex", 500.00, 0.01));
+            list.Add(new BusinessAccount(1002, "Maria", 500.0, 400.0));
+            list.Add(new SavingsAccount(1004, "Bob", 500.0, 0.01));
+            list.Add(new BusinessAccount(1005, "Anna", 500.0, 500.0));
 
-            Console.WriteLine(acc1.Balance);
-            Console.WriteLine(acc2.Balance);
+            double sum = 0.0;
+            foreach (Account acc in list)
+            {
+                sum += acc.Balance;
+            }
+
+            Console.WriteLine("Total balance: " + sum.ToString("F2", CultureInfo.InvariantCulture));
+
+            foreach (Account acc in list)
+            {
+                acc.Withdraw(10.0);
+            }
+            foreach (Account acc in list)
+            {
+                Console.WriteLine("Updated balance for account "
+                    + acc.Number
+                    + ": "
+                    + acc.Balance.ToString("F2", CultureInfo.InvariantCulture));
+            }
         }
     }
 }
