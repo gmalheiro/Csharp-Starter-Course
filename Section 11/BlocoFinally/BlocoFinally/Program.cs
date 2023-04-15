5cf99cc9a2a5028e380e56dc0d5e847a5704839e
Gabriel Malheiro <g.silva.malheiro@avanade.com>
2023-04-15 18:47:54 -0300

    Class With solution very bad done

---
+++ Section 11/BlocoFinally/BlocoFinally/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace BlocoFinally
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            FileStream fs = null;
+            try
+            {
+                fs = new FileStream(@"C:\temp\data.txt", FileMode.Open);
+                StreamReader sr = new StreamReader(fs);
+                string line = sr.ReadLine();
+                Console.WriteLine(line);
+            }
+            catch (FileNotFoundException e)
+            {
+                Console.WriteLine($"You don't have this file in your system! {e.Message}");
+            }
+            finally
+            {
+                if (fs != null)
+                {
+                    fs.Close();
+                }
+
+            }
+        }
+    }
+}
---
+++ Section 11/CriandoExcecoesPersonalizadas/CriandoExcecoesPersonalizadas/Entities/Reservation.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CriandoExcecoesPersonalizadas.Entities
+{
+    public class Reservation
+    {
+        public int RoomNumber { get; set; }
+        public DateTime CheckIn { get; set; }
+        public DateTime CheckOut { get; set; }
+
+        public Reservation()
+        {
+            
+        }
+
+        public Reservation(int roomNumber, DateTime checkin, DateTime checkout)
+        {
+            RoomNumber = roomNumber;
+            CheckIn = checkin;
+            CheckOut = checkout;
+        }
+
+        public int Duration()
+        {
+            TimeSpan duration = CheckOut.Subtract(CheckIn);
+            return (int)duration.TotalDays;
+        }
+
+        public void UpdateDates(DateTime checkin, DateTime checkout)
+        {
+            CheckIn = checkin;
+            CheckOut = checkout;
+        }
+
+        public override string ToString()
+        {
+            return $"Room {RoomNumber} , check-in {CheckIn.ToString("dd/MM/yyyy")}, check-out: {CheckOut.ToString("dd/MM/yyyy")}, {Duration()} nights";
+        }
+    }
+}
