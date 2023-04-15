2ffd7bbf6e9d3b6489610eb12c76df88e70ab005
Gabriel Malheiro <g.silva.malheiro@avanade.com>
2023-04-15 19:01:09 -0300

    Criando Excecoes Personalizadas Ruim done

---
+++ Section 11/CriandoExcecoesPersonalizadasRuim/CriandoExcecoesPersonalizadasRuim/Entities/Reservation.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CriandoExcecoesPersonalizadasRuim.Entities
+{
+    public class Reservation
+    {
+        public int RoomNumber { get; set; }
+        public DateTime CheckIn { get; set; }
+        public DateTime CheckOut { get; set; }
+
+        public Reservation()
+        {
+
+        }
+
+        public Reservation(int roomNumber, DateTime checkin, DateTime checkout)
+        {
+            RoomNumber = roomNumber;
+            CheckIn = checkin;
+            CheckOut = checkout;
+        }
+
+        public int Duration()
+        {
+            TimeSpan duration = CheckOut.Subtract(CheckIn);
+            return (int)duration.TotalDays;
+        }
+
+        public string UpdateDates(DateTime checkin, DateTime checkout)
+        {
+
+            DateTime now = DateTime.Now;
+            if (checkin < now || checkout < now)
+            {
+                return "Error in reservation: Reservation dates for update must be future dates";
+            }
+            if (checkout <= checkin)
+            {
+                return "Error in reservation: Check-Out must be after check-in in date" ;
+            }
+
+            CheckIn = checkin;
+            CheckOut = checkout;
+            return null;
+        }
+
+        public override string ToString()
+        {
+            return $"Room {RoomNumber} , check-in {CheckIn.ToString("dd/MM/yyyy")}, check-out: {CheckOut.ToString("dd/MM/yyyy")}, {Duration()} nights";
+        }
+    }
+}
--- Section 11/CriandoExcecoesPersonalizadasRuim/CriandoExcecoesPersonalizadasRuim/Program.cs
+++ Section 11/CriandoExcecoesPersonalizadasRuim/CriandoExcecoesPersonalizadasRuim/Program.cs
@@ -1,10 +1,49 @@
-﻿namespace CriandoExcecoesPersonalizadasRuim
+﻿using CriandoExcecoesPersonalizadasRuim.Entities;
+
+namespace CriandoExcecoesPersonalizadasRuim
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Solução Muito ruim!
+
+            Console.Write("Room Number:");
+            int roomNumber = int.Parse(Console.ReadLine());
+            Console.Write("Check-in date (dd/MM/yyyy):");
+            DateTime checkIn = DateTime.Parse(Console.ReadLine());
+            Console.Write("Check-out date (dd/MM/yyyy):");
+            DateTime checkOut = DateTime.Parse(Console.ReadLine());
+
+            if (checkOut <= checkIn)
+            {
+                Console.WriteLine("Error in reservation: Check-Out must be after check-in in date");
+            }
+            else
+            {
+                Reservation reservation = new Reservation(roomNumber, checkIn, checkOut);
+                Console.WriteLine("Reservation: " + reservation);
+
+                Console.WriteLine();
+                Console.WriteLine("Enter data to update the reservation:");
+                Console.Write("Check-in date (dd/MM/yyyy):");
+                checkIn = DateTime.Parse(Console.ReadLine());
+                Console.Write("Check-out date (dd/MM/yyyy):");
+                checkOut = DateTime.Parse(Console.ReadLine());
+
+                string error = reservation.UpdateDates(checkIn, checkOut);
+
+                if (error != null)
+                {
+                    Console.WriteLine($"Error in reservation {error}");
+                }
+                else
+                {
+                    Console.WriteLine("Reservation:" + reservation);
+                }
+
+            }
+
         }
     }
 }
