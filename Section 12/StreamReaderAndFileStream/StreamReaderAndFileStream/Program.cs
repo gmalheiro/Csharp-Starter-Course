27a9e7d6bf381c98f9d5b365acfa66e6180dc7a5
Gabriel Malheiro <g.silva.malheiro@avanade.com>
2023-05-06 14:16:50 -0300

    Working with files module

---
+++ Section 12/StreamReaderAndFileStream/StreamReaderAndFileStream/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace StreamReaderAndFileStream
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string? path = @"C:\Working-with-files\file1.txt";
+            FileStream? fileStream = null;
+            StreamReader? streamReader = null;
+            try
+            {
+                fileStream = new FileStream(path, FileMode.Open);
+                streamReader = new StreamReader(fileStream   );
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);   
+            }
+
+        }
+    }
+}
---
+++ Section 12/WorkingWithFiles/WorkingWithFiles/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.IO;
+namespace WorkingWithFiles
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string sourcePath = @"C:\Working-with-files\file1.txt";
+            string targetPath = @"C:\Working-with-files\file4.txt";
+             
+            try
+            {
+                FileInfo fileInfo = new FileInfo(sourcePath);
+                fileInfo.CopyTo(targetPath);
+                Console.WriteLine("File copied!");
+                string [] lines = File.ReadAllLines(sourcePath);
+                foreach (string line in lines)
+                {
+                    Console.WriteLine(line);
+                }
+            }
+            catch(IOException e)
+            {
+                Console.WriteLine("An error occured");
+                Console.WriteLine(e.Message);
+            }
+        
+        }
+    }
+}
