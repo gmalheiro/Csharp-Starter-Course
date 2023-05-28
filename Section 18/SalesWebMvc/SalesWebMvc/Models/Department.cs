a039a4409fbdc4579e377d8471caa3c9ac55e9ba
Gabriel Malheiro <g.silva.malheiro@avanade.com>
2023-05-28 19:21:56 -0300

    Other Entities and second migration

--- Section 18/SalesWebMvc/SalesWebMvc/Models/Department.cs
+++ Section 18/SalesWebMvc/SalesWebMvc/Models/Department.cs
@@ -4,16 +4,32 @@
     {
         public int Id { get; set; }
         public string? Name { get; set; }
+        public ICollection<Seller> Sellers { get; set; } = new List<Seller>();
 
         public Department()
         {
-                
         }
 
         public Department(int id, string name)
         {
             Id = id;
             Name = name;
+        }
+
+        public void AddSeller (Seller seller)
+        {
+            if (seller == null)
+            {
+                throw new ArgumentNullException(nameof(seller));
+            }
+
+            Sellers.Add(seller);
+        }
+
+        public double TotalSales(DateTime initial, DateTime final)
+        {
+            var totalSales = Sellers.Sum(seller => seller.TotalSales(initial,final));
+            return totalSales;
         }
 
     }
